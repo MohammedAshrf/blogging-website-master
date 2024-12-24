45e805e36576ca7aa92d21b0ee31f1cb2c59f98b
Momamed ashraf <90mohamedashraf@gmail.com>
2024-12-24 16:43:31 +0200

    update files 1

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <div>Footer component</div>;
+  return (
+    <div className="bg-backgroundColor text-textColor ps-6 py-2">
+      by Mohamed Ashraf &copy;2024
+    </div>
+  );
 }
